3e060b5597612bc63623c1af983b570c0a269570
Igor Fernandes Vicente <igorfvicente@gmail.com>
2024-11-18 15:44:58 -0300

    correção no método de chamada do teste

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -8,13 +9,27 @@ import (
 
 	"github.com/IgorFVicente/api-go-gin/controllers"
 	"github.com/IgorFVicente/api-go-gin/database"
+	"github.com/IgorFVicente/api-go-gin/models"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "12345678901", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -32,9 +47,12 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 func TestListandoTodosOsAlunosHandler(t *testing.T) {
 	database.ConectaComBancoDeDados()
 	r := SetupDasRotasDeTeste()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
-	req, _ := http.NewRequest("Get", "/alunos", nil)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code)
+	fmt.Println(resposta.Body)
 }
